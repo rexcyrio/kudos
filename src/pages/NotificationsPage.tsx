1ce6a45993d4c21eb94c2921024413752c3608ed
rexcyrio <stefancyrio@gmail.com>
2023-06-18 16:35:05 +0800

    wip: connect React and Firebase

--- src/pages/NotificationsPage.tsx
+++ src/pages/NotificationsPage.tsx
@@ -1,8 +1,31 @@
-import React from "react";
-import { Text } from "react-native";
+import { collection, onSnapshot, query } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import { Text, View } from "react-native";
+import { db } from "../../firebase";
 
 function NotificationsPage(): JSX.Element {
-  return <Text>This is the notifications page</Text>;
+  const [text, setText] = useState("");
+
+  useEffect(() => {
+    const q = query(collection(db, "users"));
+
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      const posts = querySnapshot.docs.map((doc, index) => {
+        return doc.data();
+      });
+
+      setText(posts[0].name);
+    });
+
+    return unsubscribe;
+  }, []);
+
+  return (
+    <View>
+      <Text>This is the notifications page</Text>
+      <Text>{text}</Text>
+    </View>
+  );
 }
 
 export default React.memo(NotificationsPage);
