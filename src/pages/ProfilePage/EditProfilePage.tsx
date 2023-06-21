23250b716738df94169e88d6e39c1b8fb416f67c
gekhro <jethrosimsy+bitbucket@gmail.com>
2023-06-21 14:53:45 +0800

    add: UI for edit profiile page

--- src/pages/ProfilePage/EditProfilePage.tsx
+++ src/pages/ProfilePage/EditProfilePage.tsx
@@ -1,14 +1,11 @@
-import React, { useState, useEffect } from "react";
-import {
-  Image,
-  View,
-  Text,
-  TouchableOpacity,
-  TextInput,
-  Button,
-} from "react-native";
+import React, { useCallback, useEffect, useRef, useState } from "react";
+import { Image, View, Text, TouchableOpacity, Button } from "react-native";
+import { TextInput } from "react-native-paper";
 import { launchImageLibrary, MediaType } from "react-native-image-picker";
+import { Feather } from "@expo/vector-icons";
 import { styles } from "./EditProfilePageStyles";
+import { db } from "../../firebase";
+import { Person, PersonProfilePageProps } from "../utilities/types";
 
 const EditProfilePage = ({
   name,
@@ -18,6 +15,15 @@ const EditProfilePage = ({
   setJobTitle,
 }) => {
   const [profilePic, setProfilePic] = useState(null);
+  const [person, setPerson] = useState<Person | null>(null);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [userInput, setUserInput] = useState("");
+  const [helperText, setHelperText] = useState("");
+
+  const [isSnackbarVisible, setIsSnackbarVisible] = useState(false);
+  const [transactionErrorMessage, setTransactionErrorMessage] = useState("");
+
+  const ref = useRef<React.ReactElement>(null);
 
   const handleProfilePicChange = () => {
     const options = {
@@ -48,25 +54,52 @@ const EditProfilePage = ({
   };
 
   return (
-    <View>
+    <View style={styles.container}>
       <TouchableOpacity onPress={handleProfilePicChange}>
-        {profilePic ? (
-          <Image style={styles.profileImg} source={{ uri: profilePic }} />
-        ) : (
-          <Image
-            style={styles.profileImg}
-            source={require("../../../assets/profile_img1.png")}
-          />
-        )}
-        <Text>Change Profile Picture</Text>
+        <View style={styles.profileImgContainer}>
+          <View style={styles.profileAction}>
+            <Feather name="edit-3" size={15} color="#fff" />
+          </View>
+          {profilePic ? (
+            <Image style={styles.profileImg} source={{ uri: profilePic }} />
+          ) : (
+            <Image
+              style={styles.profileImg}
+              source={require("../../../assets/profile_img1.png")}
+            />
+          )}
+        </View>
       </TouchableOpacity>
-      <TextInput placeholder="Name" value={name} onChangeText={setName} />
-      <TextInput
-        placeholder="Job Description"
-        value={jobTitle}
-        onChangeText={setJobTitle}
-      />
-      <Button title="Save Changes" onPress={handleSaveChanges} />
+      <Text style={styles.prompt}>
+        Edit name <Text style={{ fontWeight: "bold" }}>{person?.name}</Text>
+      </Text>
+      <View style={styles.inputContainer}>
+        <TextInput
+          inputMode="text"
+          autoFocus={true}
+          placeholder="Name"
+          value={name}
+          onChangeText={setName}
+          style={styles.textInput}
+        />
+      </View>
+      <Text style={styles.prompt}>
+        Edit job description{" "}
+        <Text style={{ fontWeight: "bold" }}>{person?.name}</Text>
+      </Text>
+      <View style={styles.inputContainer}>
+        <TextInput
+          inputMode="text"
+          autoFocus={true}
+          placeholder="Job Description"
+          value={jobTitle}
+          onChangeText={setJobTitle}
+          style={styles.textInput}
+        />
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button title="Save Changes" onPress={handleSaveChanges} />
+      </View>
     </View>
   );
 };
